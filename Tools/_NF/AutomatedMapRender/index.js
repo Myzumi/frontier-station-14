2450d374fc7cb3f641d7bb50f8ad6f287c9971e9
Myzumi <minecaft837@gmail.com>
2025-05-08 18:31:40 +0200

    Even more for Linux

--- Tools/_NF/AutomatedMapRender/index.js
+++ Tools/_NF/AutomatedMapRender/index.js
@@ -233,7 +233,7 @@ function AddExecLogs(exec, prefix = null, shuttle = null) {
 }
 
 function RenameMappedFile(shuttle) {
-  const ShipyardPath = path.join(__dirname, "ShuttleRenders", shuttle);
+  let ShipyardPath = path.join(__dirname, "ShuttleRenders", shuttle);
   const ShuttleName = shuttle.split(".")[0];
   let ShuttleFile = path.join(ShipyardPath, `${ShuttleName}-0.png`);
   const ShuttleFileNew = path.join(ShipyardPath, `${ShuttleName}.png`);
@@ -241,6 +241,7 @@ function RenameMappedFile(shuttle) {
     fs.renameSync(ShuttleFile, ShuttleFileNew);
   } else {
     // The Linux version seem to uppercase the first letter of the shuttle name
+    ShipyardPath = path.join(__dirname, "ShuttleRenders", shuttle.replace(/^./, str => str.toUpperCase()));
     ShuttleFile = path.join(ShipyardPath, `${ShuttleName.replace(/^./, str => str.toUpperCase())}-0.png`);
     if (fs.existsSync(ShuttleFile)) {
       fs.renameSync(ShuttleFile, ShuttleFileNew);
